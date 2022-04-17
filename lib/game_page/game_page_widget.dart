61d3a90a285f43c1089d39c5414155393c54488c
Shirley Alus <shirleyal@mta.ac.il>
2022-04-17 21:30:42 +0300

    ui update

--- lib/game_page/game_page_widget.dart
+++ lib/game_page/game_page_widget.dart
@@ -15,39 +15,18 @@ class _GamePageWidgetState extends State<GamePageWidget>
     with TickerProviderStateMixin {
   final animationsMap = {
     'containerOnPageLoadAnimation': AnimationInfo(
-      trigger: AnimationTrigger.onPageLoad,
-      duration: 1000,
-      delay: 1000,
-      fadeIn: true,
-      initialState: AnimationState(
-        opacity: 0,
-      ),
-      finalState: AnimationState(
-        opacity: 1,
-      ),
-    ),
-    'columnOnPageLoadAnimation': AnimationInfo(
-      trigger: AnimationTrigger.onPageLoad,
-      duration: 100,
-      fadeIn: true,
-      initialState: AnimationState(
-        opacity: 0,
-      ),
-      finalState: AnimationState(
-        opacity: 1,
-      ),
-    ),
-    'textOnPageLoadAnimation': AnimationInfo(
+      curve: Curves.easeIn,
       trigger: AnimationTrigger.onPageLoad,
       duration: 600,
-      delay: 1100,
       fadeIn: true,
       initialState: AnimationState(
-        offset: Offset(0, 70),
+        offset: Offset(0, 0),
+        scale: 1,
         opacity: 0,
       ),
       finalState: AnimationState(
         offset: Offset(0, 0),
+        scale: 1,
         opacity: 1,
       ),
     ),
@@ -70,34 +49,97 @@ class _GamePageWidgetState extends State<GamePageWidget>
       key: scaffoldKey,
       backgroundColor: Color(0xFF1E2429),
       body: Container(
-        width: double.infinity,
-        height: double.infinity,
+        width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [Color(0xFF4B39EF), Color(0xFFEE8B60)],
+            colors: [Color(0xFFB877FF), Color(0xFFFBAB66)],
             stops: [0, 1],
-            begin: AlignmentDirectional(1, -1),
-            end: AlignmentDirectional(-1, 1),
+            begin: AlignmentDirectional(0.64, 1),
+            end: AlignmentDirectional(-0.64, -1),
           ),
         ),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
-              child: Text(
-                'APP',
-                style: FlutterFlowTheme.of(context).title1.override(
-                      fontFamily: 'Lexend Deca',
-                      color: Colors.white,
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(0, 40, 0, 0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 1000,
+                      constraints: BoxConstraints(
+                        maxHeight: 100,
+                      ),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 6,
+                            color: Colors.black,
+                          )
+                        ],
+                        gradient: LinearGradient(
+                          colors: [Color(0xFF693B2F), Color(0xFF905545)],
+                          stops: [0, 1],
+                          begin: AlignmentDirectional(0, -1),
+                          end: AlignmentDirectional(0, 1),
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                        shape: BoxShape.rectangle,
+                      ),
+                      alignment: AlignmentDirectional(0, 0),
+                      child: Text(
+                        'Score Board\n\$ : \$',
+                        textAlign: TextAlign.center,
+                        style: FlutterFlowTheme.of(context).bodyText1.override(
+                              fontFamily: 'Poppins',
+                              color: Colors.white,
+                            ),
+                      ),
                     ),
-              ).animated([animationsMap['textOnPageLoadAnimation']]),
-            ),
-          ],
-        ).animated([animationsMap['columnOnPageLoadAnimation']]),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [],
+                ),
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Color(0x65EEEEEE),
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(0),
+                          bottomRight: Radius.circular(0),
+                          topLeft: Radius.circular(1000000),
+                          topRight: Radius.circular(1000000),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ).animated([animationsMap['containerOnPageLoadAnimation']]),
     );
   }
