9f2cf5426263fb8871917cc869d99556e3992363
idanhauser <idanhauser@users.noreply.github.com>
2022-04-26 18:45:14 +0300

    showing two cards with symbols.

--- lib/backend/engine/game_card.dart
+++ lib/backend/engine/game_card.dart
@@ -1,5 +1,104 @@
-class GameCard{
-  var symbols = <Symbol>[];
-  var symbolsInt = <int>[];
-//  Card(this.symbols);
+import 'package:flutter/material.dart';
+import 'package:strike_d_type/backend/engine/Game.dart';
+
+class GameCard extends StatefulWidget {
+  GameCard({Key key, this.cardSymbols}) : super(key: key);
+  final List<int> cardSymbols;
+
+  @override
+  State<GameCard> createState() => _GameCardState();
+}
+
+class _GameCardState extends State<GameCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Table(
+      border: TableBorder.all(),
+      columnWidths: const <int, TableColumnWidth>{
+        0: IntrinsicColumnWidth(),
+        1: IntrinsicColumnWidth(),
+        2: IntrinsicColumnWidth(),
+      },
+      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+      children: <TableRow>[
+        TableRow(
+          decoration: const BoxDecoration(
+            color: Colors.grey,
+          ),
+          children: <Widget>[
+            Container(
+              //mainAxisAlignment: MainAxisAlignment.spaceAround,
+              child: Icon(
+                Icons.favorite,
+                color: Colors.pink,
+                size: 24.0,
+              ),
+            ),
+            TableCell(
+              verticalAlignment: TableCellVerticalAlignment.top,
+              child: Container(
+                height: 64,
+                width: 32,
+                color: Colors.yellow,
+                child: Icon(
+                  Icons.icecream_sharp,
+                  color: Colors.blue,
+                  size: 24.0,
+                ),
+              ),
+            ),
+            Container(
+              height: 64,
+              width: 32,
+              color: Colors.black,
+              child: Icon(
+                Icons.shuffle,
+                color: Colors.pinkAccent,
+                size: 24.0,
+              ),
+            ),
+          ],
+        ),
+        TableRow(
+          decoration: const BoxDecoration(
+            color: Colors.grey,
+          ),
+          children: <Widget>[
+            Container(
+              height: 64,
+              width: 32,
+              color: Colors.deepOrange,
+              child: Icon(
+                Icons.accessibility_new_sharp,
+                color: Colors.green,
+                size: 24.0,
+              ),
+            ),
+            Container(
+              height: 64,
+              width: 32,
+              color: Colors.blue,
+              child: Icon(
+                Icons.score,
+                color: Colors.redAccent,
+                size: 24.0,
+              ),
+            ),
+            Center(
+              child: Container(
+                height: 64,
+                width: 32,
+                color: Colors.amber,
+                child: Icon(
+                  Icons.ac_unit_sharp,
+                  color: Colors.black12,
+                  size: 24.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
 }
--- lib/game_page/components/playing_card.dart
+++ lib/game_page/components/playing_card.dart
@@ -7,19 +7,15 @@ class PlayingCard extends StatelessWidget {
   final double size;
   final bool visible;
   final Color color;
-  const PlayingCard({Key key, this.card, this.size = 1, this.visible = false, this.color})
+
+  const PlayingCard(
+      {Key key, this.card, this.size = 1, this.visible = false, this.color})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: CARD_WIDTH * size,
-      height: CARD_HEIGHT * size,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(4.0),
-        color: color,
-      ),
-
+      child: GameCard(cardSymbols: [],),
     );
   }
 }
