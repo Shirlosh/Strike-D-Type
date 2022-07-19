2e0e238451a85ab4b0a0598f2797c2033d72142d
idanhauser <idanhauser@users.noreply.github.com>
2022-07-19 17:33:10 +0300

    symbols now have different size and rotations

--- lib/game_page/components/card/GameCard.dart
+++ lib/game_page/components/card/GameCard.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'Cell.dart';
+import 'dart:math';
 
 class GameCard extends StatefulWidget {
-  GameCard(this.cardSymbols)
-  {
+  GameCard(this.cardSymbols) {
     rows = (cardSymbols.length / cols).ceil();
   }
+
   final List<int> cardSymbols;
+
   int rows;
   final cols = 3;
+  Random random = new Random();
+  List<int> randomNumbers = [];
 
   @override
   State<GameCard> createState() => _GameCardState();
 }
+
 // todo: fit table to picture
 class _GameCardState extends State<GameCard> {
   @override
@@ -22,27 +27,34 @@ class _GameCardState extends State<GameCard> {
         height: 350,
         margin: EdgeInsets.all(10),
         padding: EdgeInsets.all(5),
-      decoration: BoxDecoration(
+        decoration: BoxDecoration(
           image: DecorationImage(
-              fit: BoxFit.fill,
-              image: Image.asset('assets/images/GameCard.png',).image
-          ),
-      ),
+              fit: BoxFit.fitHeight,
+              image: Image
+                  .asset(
+                'assets/images/GameCard.png',
+              )
+                  .image),
+        ),
         child: Table(
+            columnWidths: const <int, TableColumnWidth>{
+              0: IntrinsicColumnWidth(),
+              1: IntrinsicColumnWidth(),
+              2: IntrinsicColumnWidth(),
+            },
+            children: <TableRow>[
+        for (int i = 0, index = 0; i < widget.rows; i++)
+        TableRow(children: <Widget>[
+    for (int j = 0; j < widget.cols; j++, index++)
+    RotatedBox(
+        quarterTurns: widget.random.nextInt(100),
+            child: Transform.scale(scale:widget.random.nextInt(2).toDouble()+1,
+                child: Cell(widget.cardSymbols[index]))),
 
-      columnWidths: const <int, TableColumnWidth>{
-        0: IntrinsicColumnWidth(),
-        1: IntrinsicColumnWidth(),
-        2: IntrinsicColumnWidth(),
-      },
-      children: <TableRow>[
-        for(int i = 0, index = 0 ; i < widget.rows ; i++)
-          TableRow(
-            children: <Widget>[ for(int j = 0 ; j < widget.cols ; j++, index++) Cell(widget.cardSymbols[index])]
-                // todo: rotate icons
-          ),
-      ],
-    ));
 
+        ]
+    )],
+    )
+    );
   }
 }
