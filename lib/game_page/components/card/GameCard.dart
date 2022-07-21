41947b26ebccdf38bf9b460535c4f804e6273eb7
idanhauser <idanhauser@users.noreply.github.com>
2022-07-21 15:33:16 +0300

    formatting code

--- lib/game_page/components/card/GameCard.dart
+++ lib/game_page/components/card/GameCard.dart
@@ -23,50 +23,37 @@ class _GameCardState extends State<GameCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: 500,
-        height: 500,
-     // color: Colors.blue[600],
-
-
-        margin: EdgeInsets.all(20),
-        padding: EdgeInsets.all(5),
-        decoration:
-        BoxDecoration(
-
-
-          image: DecorationImage(
-              fit: BoxFit.fill,
-              image:
-              Image
-                  .asset(
-                'assets/images/GameCard.png',
-              )
-                  .image),
-
-        ),
-      alignment:Alignment.center,
-        child: Table(
-            columnWidths: const <int, TableColumnWidth>{
-              0: IntrinsicColumnWidth(),
-              1: IntrinsicColumnWidth(),
-              2: IntrinsicColumnWidth(),
-            },
-
-            children: <TableRow>[
-
-              for (int i = 0, index = 0; i < widget.cols; i++)
-        TableRow(children: <Widget>[
-          for (int j = 0; j < widget.rows; j++, index++)
-    Transform(
-        transform: Matrix4.rotationZ(widget.random.nextInt(1000)/100),
-            child: Transform.scale(scale:widget.random.nextInt(2).toDouble()+1,
-                child: Cell(widget.cardSymbols[index]))),
-
-
-
-        ]
-    )],
-    ),);
-
+      width: 500,
+      height: 500,
+      margin: EdgeInsets.all(20),
+      padding: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            fit: BoxFit.fill,
+            image: Image.asset(
+              'assets/images/GameCard.png',
+            ).image),
+      ),
+      alignment: Alignment.center,
+      child: Table(
+        columnWidths: const <int, TableColumnWidth>{
+          0: IntrinsicColumnWidth(),
+          1: IntrinsicColumnWidth(),
+          2: IntrinsicColumnWidth(),
+        },
+        children: <TableRow>[
+          for (int i = 0, index = 0; i < widget.cols; i++)
+            TableRow(children: <Widget>[
+              for (int j = 0; j < widget.rows; j++, index++)
+                Transform(
+                    transform:
+                        Matrix4.rotationZ(widget.random.nextInt(1000) / 100),
+                    child: Transform.scale(
+                        scale: widget.random.nextInt(2).toDouble() + 1,
+                        child: Cell(widget.cardSymbols[index]))),
+            ])
+        ],
+      ),
+    );
   }
 }
